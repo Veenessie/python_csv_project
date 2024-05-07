b0d817cbefff6872e3cba210b551dcbdf3e4e734
Verity <mastersverity@gmail.com>
2024-05-07 12:20:13 +0100

    manipulated CSV data in continent, population and years

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 
 file_name = 'data.csv'
 
+population_per_continent = {}
+
 with open(file_name, 'r') as csvfile:
   reader = csv.DictReader(csvfile)
 
@@ -11,12 +13,18 @@ with open(file_name, 'r') as csvfile:
     continent = line['continent']
     year = line['year']
     population = line['population']
-    print(continent)
-    print(year)
-    print(population)
 
+    if continent not in population_per_continent:
+      population_per_continent[continent] = {'population': [], 'years': []}
 
-plt.plot([2000, 2010, 2020, 2030], [100, 200, 300, 400], label="Europe", marker ="x")
+    population_per_continent[continent]['population'].append(population)
+    population_per_continent[continent]['years'].append(year)
+
+print(population_per_continent)
+
+plt.plot([2000, 2010, 2020, 2030], [100, 200, 300, 400],
+         label="Europe",
+         marker="x")
 
 plt.title("Internet Population Per Continent")
 plt.xlabel("Year")
